d273b04a893028958edfc2a8f57c8d62f07e68f0
Hamza162004 <143029327+Hamza162004@users.noreply.github.com>
2024-03-17 01:36:10 +0500

    gave transition delay

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -15,7 +15,7 @@ const Services = () => {
                 <img className='card-image ' src={coding} alt="" />
                 <div className="content">
                 <div className='flex justify-center '>
-                        <h1 className='text-2xl font-semibold my-2'>Website Development</h1>
+                        <h1 className='text-2xl font-semibold my-2 sm:text-lg'>Website Development</h1>
                         <svg className='svg' style={{width:'20px'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.383 8.076a1 1 0 0 0-1.09.217l-6 6A1 1 0 0 0 9 16h6a1 1 0 0 0 1-1V9a1 1 0 0 0-.617-.924z" style={{fill:'#ff8e31'}} data-name="Serong kanan bawah"/></svg>
                     </div>
                     <p>As a full-stack web developer, I specialize in crafting dynamic websites with expertise in both front-end and back-end technologies. I ensure seamless and responsive web experiences, tailored to meet unique requirements.</p>
@@ -25,7 +25,7 @@ const Services = () => {
                 <img className='card-image ' src={design} alt="" />
                 <div className="content">
                 <div className='flex justify-center '>
-                        <h1 className='text-2xl font-semibold my-2'>UI/UX Designing</h1>
+                        <h1 className='text-2xl font-semibold my-2 sm:text-lg'>UI/UX Designing</h1>
                         <svg className='svg' style={{width:'20px'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.383 8.076a1 1 0 0 0-1.09.217l-6 6A1 1 0 0 0 9 16h6a1 1 0 0 0 1-1V9a1 1 0 0 0-.617-.924z" style={{fill:'#ff8e31'}} data-name="Serong kanan bawah"/></svg>
                     </div>
                     <p> From user-friendly interfaces to engaging experiences, I bring a keen eye for design that enhances the overall user journey, making your digital presence both visually appealing and user-centric.</p>
@@ -35,7 +35,7 @@ const Services = () => {
                 <img className='card-image ' src={communication} alt="" />
                 <div className="content">
                 <div className='flex justify-center '>
-                        <h1 className='text-2xl font-semibold my-2'>Collaboration</h1>
+                        <h1 className='text-2xl font-semibold my-2 sm:text-lg'>Collaboration</h1>
                         <svg className='svg' style={{width:'20px'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.383 8.076a1 1 0 0 0-1.09.217l-6 6A1 1 0 0 0 9 16h6a1 1 0 0 0 1-1V9a1 1 0 0 0-.617-.924z" style={{fill:'#ff8e31'}} data-name="Serong kanan bawah"/></svg>
                     </div>
                     <p>I prioritize open communication and collaboration with customers, ensuring a transparent environment. This approach guarantees the delivery of effective, tailored web solutions that exceed expectations.</p>
@@ -45,7 +45,7 @@ const Services = () => {
                 <img className='card-image ' src={testing} alt="" />
                 <div className="content">
                     <div className='flex justify-center '>
-                        <h1 className='text-2xl font-semibold my-2'>Testing & Debugging</h1>
+                        <h1 className='text-2xl font-semibold my-2 sm:text-lg'>Testing & Debugging</h1>
                         <svg className='svg' style={{width:'20px'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.383 8.076a1 1 0 0 0-1.09.217l-6 6A1 1 0 0 0 9 16h6a1 1 0 0 0 1-1V9a1 1 0 0 0-.617-.924z" style={{fill:'#ff8e31'}} data-name="Serong kanan bawah"/></svg>
                     </div>
                     <p>We specialize in testing and debugging to ensure your web applications meet top-quality standards for seamless functionality, high performance, and robust security, ensuring an exceptional user experience.</p>
@@ -55,7 +55,7 @@ const Services = () => {
                 <img className='card-image ' src={maintainence} alt="" />
                 <div className="content">
                 <div className='flex justify-center '>
-                        <h1 className='text-2xl font-semibold my-2'>Maintainance & Support</h1>
+                        <h1 className='text-2xl font-semibold my-2 sm:text-lg'>Maintainance & Support</h1>
                         <svg className='svg' style={{width:'20px'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.383 8.076a1 1 0 0 0-1.09.217l-6 6A1 1 0 0 0 9 16h6a1 1 0 0 0 1-1V9a1 1 0 0 0-.617-.924z" style={{fill:'#ff8e31'}} data-name="Serong kanan bawah"/></svg>
                     </div>
                     <p>I'll ensure the continuous functionality, security and performance of your website through comprehensive maintenance and support. Trust me to keep your digital assets running smoothly, allowing you to prioritize your business.</p>
